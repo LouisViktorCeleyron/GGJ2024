db16e4a1b59445bac5b553d67a14217c29f0cb63
Louis Celeyron <115717650+Louis-Celeyron-ETPA@users.noreply.github.com>
2024-01-27 11:56:28 +0100

    Base system is OK

--- Assets/Scripts/Managers/MoneyManager.cs
+++ Assets/Scripts/Managers/MoneyManager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoneyManager : Manager
 {
     private int money;
+    public TextMeshProUGUI moneyTextGUI;
+
 
     public void LoseMoney(int moneyAmount)
     {
-        this.money -= moneyAmount;
+        this.money += moneyAmount;
+        UpdateText();
+    }
 
+    private void UpdateText()
+    {
+        moneyTextGUI.text = "$ -" + money.ToString("000000");
     }
 }
